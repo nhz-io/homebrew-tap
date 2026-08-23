86c4a93a85e7429022ae7b48eb3ca0dd131e76cf
kumarajiva <dev@nhz.io>
2026-08-23 19:28:48 +0300

    Update bcdl to 2.0.2
    The binaries were built again, thus the checksums changed. A rust build
    is not the same byte for byte in a different environment.

--- Casks/bcdl.rb
+++ Casks/bcdl.rb
@@ -1,9 +1,9 @@
 cask "bcdl" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "2.0.0"
-  sha256 arm:   "207ff8c2748a9367dff07dfb9b4df8307240fdb14d53bfda3600b64962801abe",
-         intel: "85fedd23388a9cfdafa4d29785cc5a3671e8ffaf74744543e2f5528f15879319"
+  version "2.0.2"
+  sha256 arm:   "15a6ee25bc68b0da039924156bb15413e229666d4043bd72fb4f339ead754c2d",
+         intel: "6adb30a0512347100c2d522623b6a4f8dd53e9be17a56a20f58fbe541bd927b5"
 
   url "https://github.com/nhz-io/bandcamp-downloader/releases/download/v#{version}/bcdl-v#{version}-#{arch}-apple-darwin.tar.gz"
   name "bcdl"
